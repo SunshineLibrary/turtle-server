b04cc95683c4c04bff59e48b74b4b37e09430271
linuo <3er.com.cn@gmail.com>
2013-08-22 03:41:02 +0800

    [linuo] update url regex for multiple query strings without order.

--- src/main/java/sunlib/turtle/api/SunApiMatcher.java
+++ src/main/java/sunlib/turtle/api/SunApiMatcher.java
@@ -14,7 +14,7 @@ public class SunApiMatcher extends ApiRequestTypeMatcher {
     static SunApiMatcher matcher = new SunApiMatcher();
 
     private SunApiMatcher() {
-        add(GET, NanoHTTPD.Method.GET, "/exercise/v1/root\\?callback=[^&]+");
+        add(GET, NanoHTTPD.Method.GET, "/exercise/v1/root\\?(?=(.*&)?callback=[^&]+).*");
         add(GET, NanoHTTPD.Method.GET, "/pack/v1");
 
         /* Use cached content when available. Fetch update and cache on cache miss. Can block. */
@@ -22,10 +22,10 @@ public class SunApiMatcher extends ApiRequestTypeMatcher {
         // GET  http://127.0.0.1/exercise/v1/subjects/1?ts=123456789
         // GET  http://127.0.0.1/exercise/v1/chapters/1?ts=123456789
         // GET  http://127.0.0.1/exercise/v1/lessons/1?ts=123456789
-        add(GET_CACHE, NanoHTTPD.Method.GET, "/exercise/v1/(subjects|chapters|lessons)/[a-z0-9]+\\?ts=[0-9]+&callback=[^&]+");
+        add(GET_CACHE, NanoHTTPD.Method.GET, "/exercise/v1/(subjects|chapters|lessons)/[a-z0-9]+\\?(?=(.*&)?callback=[^&]+)(?=(.*&)?ts=[0-9]+).*");
 
         // GET  http://127.0.0.1/exercise/v1/lessons/1/%2E%2C%2D.jpg?ts=123456789
-        add(GET_CACHE, NanoHTTPD.Method.GET, "/exercise/v1/lessons/[a-z0-9]+/.+\\.(png|jpg|jpeg|mp4|mp3)\\?ts=[0-9]+");
+        add(GET_CACHE, NanoHTTPD.Method.GET, "/exercise/v1/lessons/[a-z0-9]+/.+\\.(png|jpg|jpeg|mp4|mp3)\\?(?=(.*&)?ts=[0-9]+).*");
 
         // GET  http://127.0.0.1/pack/v1/subjects/1?ts=123456789
         // GET  http://127.0.0.1/pack/v1/folders/1?ts=123456789
@@ -33,7 +33,7 @@ public class SunApiMatcher extends ApiRequestTypeMatcher {
         add(GET_CACHE, NanoHTTPD.Method.GET, "/pack/v1/(subjects|folders|pieces)/[a-z0-9]+\\?ts=[0-9]+");
 
         // GET  http://127.0.0.1/pack/v1/pieces/1/2e9201af2920ed0192c2.mp4?ts=123456789
-        add(GET_CACHE, NanoHTTPD.Method.GET, "/exercise/v1/lessons/[a-z0-9]+/[a-z0-9]+\\.(png|jpg|jpeg|mp4|mp3)\\?ts=[0-9]+");
+        add(GET_CACHE, NanoHTTPD.Method.GET, "/pack/v1/pieces/[a-z0-9]+/[a-z0-9]+\\.(png|jpg|jpeg|mp4|mp3)\\?(?=(.*&)?ts=[0-9]+).*");
 
         // GET  http://127.0.0.1/exercise/v1/user_data/subjects/1
         // GET  http://127.0.0.1/exercise/v1/user_data/achievements/1
@@ -59,8 +59,22 @@ public class SunApiMatcher extends ApiRequestTypeMatcher {
         // GET  http://127.0.0.1/exercise/v1/chapters/1?action=status
         // GET  http://127.0.0.1/pack/v1/folders/1?action=cache
         // GET  http://127.0.0.1/pack/v1/folders/1?action=status
-        add(BATCH_CACHE, NanoHTTPD.Method.GET, "/exercise/v1/(chapters|achievements)/[a-z0-9]+\\?ts=[0-9]+&act=(cache|status)&callback=[^&]+");
+        add(BATCH_CACHE, NanoHTTPD.Method.GET, "/exercise/v1/(chapters|achievements)/[a-z0-9]+\\?(?=(.*&)?callback=[^&]+)(?=(.*&)?act=(cache|status))(?=(.*&)?ts=[0-9]+).*");
 
+        // for one query string KEY=VALUE
+        // "URL\\?(?=(.*&)?KEY=VALUE).*"
+
+        // for two query string KEYA && KEYB
+        // "URL\\?(?=(.*&)?KEYA=VALUEA)(?=(.*&)?KEYB=VALUEB).*"
+
+        // for two query string KEYA || KEYB
+        // "URL\\?(?=((.*&)?KEYA=VALUEA)|((.*&)?KEYB=VALUEB)).*"
+
+        // for four query strings A&&B || C&&D
+        // "URL\\?(?=(?=(.*&)?A=[^&]*)(?=(.*&)?B=[^&]*)|(?=(.*&)?C=[^&]*)(?=(.*&)?D=[^&]*)).*"
+
+        // for four query strings (A|B) && (C|D)
+        // "URL\\?(?=((?=(.*&)?A=[^&]*)|(?=(.*&)?B=[^&]*)))(?=((?=(.*&)?C=[^&]*)|(?=(.*&)?D=[^&]*))).*"
     }
 
     public static SunApiMatcher getMatcher() {
